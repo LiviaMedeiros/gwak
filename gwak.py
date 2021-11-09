8e3c407c5dcd64381a61316fd26ee4c5a698307d
LiviaMedeiros <livia@cirno.name>
2021-11-09 09:27:12 +0800

    partially migrated to pathlib

--- gwak.py
+++ gwak.py
@@ -5,6 +5,7 @@ import shutil
 import collections
 import hashlib
 import logging
+from pathlib import Path
 
 __formats = ['yaml', 'json']
 try:
@@ -20,26 +21,28 @@ except ImportError:
 
 __MINSIZE = 256
 __MINDUPE = 2
-__MANIFEST = "gwak"
-__GWAK = "._gwak"
+__MANIFEST = 'gwak'
+__GWAK = '._gwak'
 
 __params = None
 
 
-def _filter_dir(path: str) -> str:
-    if not os.path.isdir(path):
-        raise NotADirectoryError(string)
-    return os.path.join(path, '')
+def _filter_dir(path: str) -> Path:
+    res = Path(path).resolve()
+    if not res.is_dir():
+        raise NotADirectoryError(path)
+    return res
 
-def _rmdir(path: str) -> bool:
-    if not os.path.isdir(path) or os.listdir(path):
+def _rmdir(path: Path) -> bool:
+    if not path.is_dir() or any(path.iterdir()):
         return False
+    logging.debug(f"rmdir [{path}]")
     if not __params.dry_run:
-        os.rmdir(path)
+        path.rmdir()
     return True
 
-def _is_not_regular_file(path: str) -> bool:
-    return not os.path.isfile(path) or os.path.islink(path)
+def _is_not_regular_file(path: Path) -> bool:
+    return not path.is_file() or path.is_symlink()
 
 def _is_smol(size: str) -> bool:
     return int(size, 16) < __params.minsize
@@ -50,43 +53,46 @@ def __format_size(body: bytes) -> str:
 def __format_hash(body: bytes) -> str:
     return hashlib.sha3_512(body).hexdigest()
 
-def _bury(size: str, hash: str, file: str) -> tuple:
-    body_dirname = os.path.join(__params.graveyard, size)
+def _bury(size: str, hash: str, file: Path) -> tuple:
+    body_dirname = __params.grave / size
     if not __params.dry_run:
-        os.makedirs(body_dirname, exist_ok = True)
-    body = os.path.join(body_dirname, hash)
-    link = (body if __params.isabs else os.path.relpath(body, os.path.dirname(file)), file)
+        body_dirname.mkdir(parents = True, exist_ok = True)
+    body = body_dirname / hash
+    link = body if __params.isabs else os.path.relpath(body, file.parent)
     logging.debug(f"symlink [{link}]")
     if not __params.dry_run:
-        if os.path.isfile(body):
-            os.remove(file)
+        if body.is_file():
+            file.unlink()
         else:
-            os.rename(file, body)
-        os.symlink(*link)
-    return link
+            file.rename(body)
+        file.symlink_to(link)
+    return {
+        'link': str(link),
+        'body': str(file)
+    }
 
-def _exhume(file: str, links: list) -> bool:
+def _exhume(file: Path, links: list) -> bool:
     logging.info(f"exhuming [{file}]")
     for link in links:
         logging.debug(f"copyfile [{link}]")
         if not __params.dry_run:
-            os.remove(link)
+            link.unlink()
             shutil.copyfile(file, link)
     if __params.force and not __params.dry_run:
         logging.debug(f"remove [{file}]")
-        os.remove(file)
+        file.unlink()
     return True
 
 def _write_manifest(data: dict) -> None | bool:
     logging.info(f"writing manifest [{__params.manifest}]")
     if __params.dry_run:
         return True
-    os.makedirs(os.path.dirname(__params.manifest), exist_ok = True)
-    if os.path.isfile(__params.manifest):
+    __params.manifest.parent.mkdir(parents = True, exist_ok = True)
+    if __params.manifest.is_file():
         _backup_manifest()
     if __params.format not in __formats:
         raise NotImplementedError(__params.format)
-    with open(__params.manifest, 'w') as f:
+    with __params.manifest.open(mode = 'w') as f:
         match __params.format:
             case 'yaml':
                 return yaml.dump(data, f)
@@ -97,7 +103,7 @@ def _read_manifest() -> dict:
     logging.info(f"reading manifest [{__params.manifest}]")
     if __params.format not in __formats:
         raise NotImplementedError(__params.format)
-    with open(__params.manifest, 'r') as f:
+    with __params.manifest.open(mode = 'r') as f:
         match __params.format:
             case 'yaml':
                 return yaml.safe_load(f)
@@ -106,24 +112,23 @@ def _read_manifest() -> dict:
 
 def _backup_manifest() -> None:
     logging.warning(f"backing up old manifest [{__params.manifest}]")
-    with open(__params.manifest, 'rb') as f:
-        hash = hashlib.sha3_512(f.read()).hexdigest()
-    return os.rename(__params.manifest, f"{__params.manifest}.{hash}")
+    hash = hashlib.sha3_512(__params.manifest.read_bytes()).hexdigest()
+    return __params.manifest.rename(f"{__params.manifest}.{hash}")
 
 def make_manifest(paths: list) -> dict:
     gwaks = collections.defaultdict(lambda: collections.defaultdict(list))
     for path in paths:
         for root, dirs, files in os.walk(path):
-            if __params.grave in dirs:
-                dirs.remove(__params.grave)
+            root = Path(root)
+            if __params.grave.name in dirs:
+                dirs.remove(__params.grave.name)
             for file in files:
-                file = os.path.join(root, file)
+                file = root / file
                 if _is_not_regular_file(file):
                     logging.info(f"skipping irregular file [{file}]")
                     continue
-                with open(file, 'rb') as f:
-                    body = f.read()
-                gwaks[__format_size(body)][__format_hash(body)].append(file)
+                body = file.read_bytes()
+                gwaks[__format_size(body)][__format_hash(body)].append(str(file.resolve()))
     return {k: dict(v) for k, v in gwaks.items()}
 
 def _dedupe(gwaks: dict):
@@ -136,21 +141,21 @@ def _dedupe(gwaks: dict):
                 logging.debug(f"skipping unique files [{hash}]")
                 continue
             for file in files:
-                yield _bury(size, hash, file)
+                yield _bury(size, hash, Path(file))
 
 def dedupe(gwaks: dict) -> list:
     return list(_dedupe(gwaks))
 
-def _redupe(gwaks: dict, grave: str):
+def _redupe(gwaks: dict, grave: Path):
     for size, gwak in gwaks.items():
-        sizedir = os.path.join(grave, size)
+        sizedir = grave / size
         for hash, links in gwak.items():
-            hashdir = os.path.join(sizedir, hash)
-            file = os.path.join(sizedir, hash)
-            if not os.path.isfile(file):
+            hashdir = sizedir / hash
+            file = sizedir / hash
+            if not file.is_file():
                 logging.debug(f"skipping missing file [{file}]")
                 continue
-            yield _exhume(file, links)
+            yield _exhume(file, (Path(link) for link in links))
             if __params.force:
                 _rmdir(hashdir)
         if __params.force:
@@ -159,9 +164,8 @@ def _redupe(gwaks: dict, grave: str):
 def redupe(gwaks: dict, grave: str) -> bool:
     return all(_redupe(gwaks, grave))
 
-def _validate_body(file: str, size: str, hash: str) -> bool:
-    with open(file, 'rb') as f:
-        body = f.read()
+def _validate_body(file: Path, size: str, hash: str) -> bool:
+    body = file.read_bytes()
     if size != __format_size(body):
         logging.warning(f"size mismatch [{file}]")
         return False
@@ -174,7 +178,8 @@ def _validate_files(gwaks: dict):
     for size, gwak in gwaks.items():
         for hash, files in gwak.items():
             for file in files:
-                if not os.path.isfile(file):
+                file = Path(file)
+                if not file.is_file():
                     logging.warning("no such file [{file}]")
                     continue
                 yield _validate_body(file, size, hash)
@@ -182,16 +187,16 @@ def _validate_files(gwaks: dict):
 def validate_files(gwaks: dict) -> bool:
     return all(_validate_files(gwaks))
 
-def _validate_grave(gwaks: dict, grave: str):
+def _validate_grave(gwaks: dict, grave: Path):
     for size, gwak in gwaks.items():
         for hash in gwak:
-            file = os.path.join(grave, size, hash)
-            if not os.path.isfile(file):
+            file = grave / size / hash
+            if not file.is_file():
                 logging.info(f"no such body [{file}]")
                 continue
             yield _validate_body(file, size, hash)
 
-def validate_grave(gwaks: dict, grave: str) -> bool:
+def validate_grave(gwaks: dict, grave: Path) -> bool:
     return all(_validate_grave(gwaks, grave))
 
 
@@ -203,22 +208,23 @@ if __name__ == '__main__':
         parser.add_argument('path', type = _filter_dir, nargs = '+', help = "target directory")
         parser.add_argument('-v', '--verbose', action = 'count', default = 0, help = "increase verbosity")
         parser.add_argument('-q', '--quiet', action = 'count', default = 0, help = "decrease verbosity")
-        parser.add_argument('-m', '--manifest', type = str, default = __MANIFEST, metavar = "FILEPATH", help = f"manifest file (default: {__MANIFEST})")
+        parser.add_argument('-m', '--manifest', type = Path, default = __MANIFEST, metavar = 'FILEPATH', help = f"manifest file (default: {__MANIFEST})")
         parser.add_argument('--format', choices = __formats, default = __formats[0], help = "manifest format")
-        parser.add_argument('-g', '--grave', type = str, default = __GWAK, metavar = "DIR", help = f"place to bury filebodies (default: {__GWAK} in first target directory)")
+        parser.add_argument('-g', '--grave', type = Path, default = __GWAK, metavar = 'DIR', help = f"place to bury filebodies (default: {__GWAK} in first target directory)")
         parser.add_argument('-f', '--force', action = 'store_true', help = "gwak rare or small files, and delete filebodies")
         parser.add_argument('-u', '--undo', '--ungwak', action = 'store_true', help = "ungwak by replacing symlinks with regular files")
-        parser.add_argument('--minsize', type = int, default = __MINSIZE, metavar = "N", help = f"minimum file size to be replaced (default: {__MINSIZE})")
-        parser.add_argument('--mindupe', type = int, default = __MINDUPE, metavar = "N", help = f"minimum file appearances to be replaced (default: {__MINDUPE})")
+        parser.add_argument('--minsize', type = int, default = __MINSIZE, metavar = 'N', help = f"minimum file size to be replaced (default: {__MINSIZE})")
+        parser.add_argument('--mindupe', type = int, default = __MINDUPE, metavar = 'N', help = f"minimum file appearances to be replaced (default: {__MINDUPE})")
         parser.add_argument('--validate', action = 'store_true', help = "validate gwaked directory")
         parser.add_argument('--check', action = 'store_true', help = "integrity check for filebodies")
         parser.add_argument('--dry-run', action = 'store_true', help = "do not write anything")
 
         __params = parser.parse_args()
         __params.verbosity = __params.verbose - __params.quiet
-        __params.isabs = os.path.isabs(__params.grave)
-        __params.graveyard = os.path.join(__params.grave, '') if __params.isabs else os.path.join(__params.path[0], __params.grave)
-        __params.manifest = __params.manifest if os.path.isabs(__params.manifest) else os.path.join(__params.graveyard, __params.manifest)
+        __params.isabs = __params.grave.is_absolute()
+        if not __params.isabs:
+            __params.grave = __params.path[0] / __params.grave
+        __params.manifest = __params.manifest if __params.manifest.is_absolute() else __params.grave / __params.manifest
 
         logging.root.setLevel(logging.root.level - __params.verbosity * 10)
 
@@ -226,9 +232,9 @@ if __name__ == '__main__':
         if __params.validate:
             return validate_files(_read_manifest())
         if __params.check:
-            return validate_grave(_read_manifest(), __params.graveyard)
+            return validate_grave(_read_manifest(), __params.grave)
         if __params.undo:
-            return redupe(_read_manifest(), __params.graveyard)
+            return redupe(_read_manifest(), __params.grave)
 
         gwaks = make_manifest(__params.path)
         _write_manifest(gwaks)
